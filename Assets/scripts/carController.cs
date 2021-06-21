761d4e8a1a117eff428be9b5c5af38be085aa1bb
Abishek-Dev <61354560+Abishek-Dev@users.noreply.github.com>
2021-06-21 19:40:40 +0530

    Add files via upload

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    public float carSpeed;
+    public float maxPos = 2.1f;
+    Vector3 position;
+    public uiManager ui;
+    bool currentPlatformAndroid = false;
+    Rigidbody2D rb;
+    public AudioManager am;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+#if UNITY_ANDROID
+                currentPlatformAndroid=true;
+#else
+        currentPlatformAndroid = false;
+#endif
+        am.carSound.Play();
+    }
+
+
+    void Start()
+    {
+        position = transform.position;
+        if (currentPlatformAndroid == true)
+        {
+            Debug.Log("Android");
+        }
+        else
+        {
+            Debug.Log("Windows");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currentPlatformAndroid == true)
+        {
+            //TouchMove();
+            AccelerometerMove();
+        }
+        else
+        {
+            position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+            position.x = Mathf.Clamp(position.x, -2.1f, 2.1f);
+            transform.position = position;
+        }
+        position = transform.position;
+        position.x = Mathf.Clamp(position.x, -2.1f, 2.1f);
+        transform.position = position;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy Car")
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+            ui.gameOverActivated();
+            am.carSound.Stop();
+        }
+    }
+
+    void AccelerometerMove()
+    {
+        float x = Input.acceleration.x;
+        Debug.Log("X=" + x);
+        if (x < -0.1f)
+        {
+            MoveLeft();
+        }
+        else if(x>0.1f)
+        {
+            MoveRight();
+        }
+        else
+        {
+            SetVelocityZero();
+        }
+    }
+
+    void TouchMove()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            float middle = Screen.width / 2;
+            if (touch.position.x < middle && touch.phase == TouchPhase.Began)
+            {
+                MoveLeft();
+            }
+            else if (touch.position.x > middle && touch.phase == TouchPhase.Began)
+            {
+                MoveRight();
+            }
+        }
+        else
+        {
+            SetVelocityZero();
+        }
+    }
+
+    public void MoveLeft()
+    {
+        rb.velocity = new Vector2(-carSpeed, 0);
+    }
+    public void MoveRight()
+    {
+        rb.velocity = new Vector2(carSpeed, 0);
+    }
+    public void SetVelocityZero()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/scripts/carSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSpawner : MonoBehaviour
+{
+    public GameObject[] cars;
+    int carNo;
+    public float maxPos = 2.06f;
+    public float delayTimer = 3f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = delayTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0){
+            Vector3 carPos = new Vector3(Random.Range(-2.22f, 2.06f), transform.position.y, transform.position.z);
+            carNo = Random.Range(0, 8);
+            Instantiate(cars[carNo], carPos, transform.rotation);
+            timer = delayTimer;
+        }
+    }
+}
